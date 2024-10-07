e560e895c11ff536eae253aae2742bb920617e2b
johnvmp275 <marquesjoaov1@gmail.com>
2024-10-07 20:47:21 -0300

    remodelamento-arquivos

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
+include("app/Database/connect.php");
+
 //OBTÉM A ROTA DA URL
-function obterRota() {
+function obterRota(){
     $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $rota = explode("/", trim($url, '/'));
 
     //DEFINE A ROTA PADRÃO CASO ESTIVER VAZIA
     if (empty($rota[0])) {
-        $rota[0] = 'home';
+        $rota[0] = 'pagina-inicial';
     }
 
     //RETORNA A ROTA
@@ -15,7 +17,7 @@ function obterRota() {
 }
 
 //INCLUI AS PÁGINAS RETORNADAS PELO OBTERROTA()
-function incluirPagina($pagina) {
+function incluirPagina($pagina){
 
     //DEFINE O CAMINHO DO ARQUIVO DA PÁGINA A SER RENDERIZADA
     $arquivoPagina = "app/resources/{$pagina}.php";
@@ -39,12 +41,20 @@ $tituloPagina = ucfirst(str_replace('-', ' ', $rota));
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php $tituloPagina ?></title>
 </head>
+
 <body>
-    
+    <main>
+        <?php
+        //INCLUI AS PÁGINAS RETORNADAS PELO OBTER ROTA()
+        incluirPagina($rota);
+        ?>
+    </main>
 </body>
+
 </html>
