c74884ca03a133bde55a274c668db4d4e8b1d97a
johnvmp275 <marquesjoaov1@gmail.com>
2024-10-07 20:13:18 -0300

    modelamento-halaton

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
 <?php
 
+//OBTÉM A ROTA DA URL
+function obterRota() {
+    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $rota = explode("/", trim($url, '/'));
+
+    //DEFINE A ROTA PADRÃO CASO ESTIVER VAZIA
+    if (empty($rota[0])) {
+        $rota[0] = 'home';
+    }
+
+    //RETORNA A ROTA
+    return $rota[0];
+}
+
+//INCLUI AS PÁGINAS RETORNADAS PELO OBTERROTA()
+function incluirPagina($pagina) {
+
+    //DEFINE O CAMINHO DO ARQUIVO DA PÁGINA A SER RENDERIZADA
+    $arquivoPagina = "app/resources/{$pagina}.php";
+
+    //VERIFICA SE O ARQUIVO DA PÁGINA NÃO EXISTE
+    if (!file_exists($arquivoPagina)) {
+        echo "Página não encontrada.";
+        return;
+    }
+
+    //RENDERIZA A PÁGINA
+    include $arquivoPagina;
+}
+
+//EXECUTA A FUNÇÃO OBTERROTA()
+$rota = obterRota();
+
+//DEFINE O TITULO DA PÁGINA
+$tituloPagina = ucfirst(str_replace('-', ' ', $rota));
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $tituloPagina ?></title>
+</head>
+<body>
+    
+</body>
+</html>
