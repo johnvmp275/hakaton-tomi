074f36072eb5e058270a3cf135b32dd1052dec42
johnvmp275 <marquesjoaov1@gmail.com>
2024-10-16 20:03:33 -0300

    nova-att

--- situacao-2/app/resources/dashboard.php
+++ situacao-2/app/resources/dashboard.php
@@ -3,3 +3,9 @@
     include('app/resources/components/faixaEtariaGrafico.php');
     ?>
 </div>
+
+<div class="form-group">
+    <?php
+    include('app/resources/components/historicoAusencias.php');
+    ?>
+</div>
